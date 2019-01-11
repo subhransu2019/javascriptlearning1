4bf9ef3179d5531da5af8302ae101081a1d490eb
subhransu2019 <subhransu219@gmail.com>
2019-01-11 15:52:49 +0530

    Trying out variables

---
+++ variables/app.js
@@ -0,0 +1,31 @@
+/*var a=10;
+var a=17;//we can reassign it
+document.write(a +"<br>");
+if(a>0)
+{
+    var a=25;
+    document.write(a +"<br>");
+}
+document.write(a +"<br>"); // the value is global
+
+let a=10;
+let a=14;//we can reassign it
+document.write(a +"<br>");
+if(a>0)
+{
+    let a=25;
+    document.write(a +"<br>");
+}
+document.write(a +"<br>");// the value is local
+
+const a=10;
+const a=14;//we can not reassign it
+document.write(a +"<br>");
+if(a>0)
+{
+    const a=25;
+    document.write(a +"<br>");
+}
+document.write(a +"<br>");// the value is local
+*/
+
